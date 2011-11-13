edb70cc0bec51f4c4105aea8518f7a785243f290
Vlad Khomich <khomich.vlad@gmail.com>
2011-11-14 01:05:49 +0200

    more

--- app/assets/javascripts/offers.js
+++ app/assets/javascripts/offers.js
@@ -1,2 +1,13 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+
+$('document').ready(function() {
+
+    $('.pagination a').live('ajax:complete', function( event, xhr, status ) {
+        var attributes = $.parseJSON( xhr.responseText );
+        $('#all-offers').html( attributes.offers );
+        $('#pagination-top').html( attributes.pagination );
+    });
+
+});
--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,7 +1,18 @@
 class OffersController < ApplicationController
 
+  layout Proc.new { |controller| controller.request.xhr?? false : 'application' }
+
   def index
     @offers = Kupongid.page( params[:page] || 1 )
+
+    respond_to do |format|
+      format.html
+      format.js do
+        render :json => { :offers => render_to_string(:partial => 'offers/offers'),
+          :pagination => render_to_string(:partial => 'offers/pagination')
+        }
+      end
+    end
   end
 
   def show
