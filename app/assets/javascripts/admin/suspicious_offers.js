0d9f9edf4cd85ae43d6b4e42a43a74fbc46816ce
Vlad Khomich <khomich.vlad@gmail.com>
2011-12-06 20:37:47 +0200

    stupid precompiling

--- app/assets/javascripts/admin/suspicious_offers.js
+++ app/assets/javascripts/admin/suspicious_offers.js
@@ -3,52 +3,54 @@
 
 $('#admin-suspicious_offers-controller').ready(function() {
 
-    $.admin = {
-        filter: $('#filter')
-    };
+    if ( $('#admin-suspicious_offers-controller').length ) {
+        $.admin = {
+            filter: $('#filter')
+        };
 
-    $.admin.filterContainer = $.admin.filter.parent();
-    $.admin.offerIdInput = $.admin.filter.find('#offer_id');
+        $.admin.filterContainer = $.admin.filter.parent();
+        $.admin.offerIdInput = $.admin.filter.find('#offer_id');
 
-    $.admin.filter.find('input[type="checkbox"]').unbind();
+        $.admin.filter.find('input[type="checkbox"]').unbind();
 
-    $('.offer').hover(
-        function() {
+        $('.offer').hover(
+            function() {
             var $this = $(this),
-                top = $this.offset().top,
-                left = $this.offset().left;
+            top = $this.offset().top,
+            left = $this.offset().left;
 
             $.admin.filterContainer.
                 show().
                 css({
-                    position: 'absolute',
-                    top: top - 100,
-                    left: (left + $this.width() + 50)
-                }).
+                position: 'absolute',
+                top: top - 100,
+                left: (left + $this.width() + 50)
+            }).
                 find('#offer_id').val( this.id.replace('offer-', ''));
         },
 
         function() {
             $.admin.filter.find('input[type="checkbox"]').filter(':checked').prop('checked', false);
         }
-    );
+        );
 
-    $.admin.filter.find('input[type="checkbox"]').bind('click', function() {
-        var offerId = $.admin.offerIdInput.val(),
-        categoryId = this.id;
+        $.admin.filter.find('input[type="checkbox"]').bind('click', function() {
+            var offerId = $.admin.offerIdInput.val(),
+            categoryId = this.id;
 
-        $.ajax({
-            type: 'PUT',
-            url: '/admin/suspicious_offers/' + offerId + '?category_id=' + categoryId,
-            success: function(data) {
-                if ( data.status == 'success' ) {
-                    $('#offer-' + offerId).find('.offer-left-side').append('<div style="color:red; font-weight:bold">UPDATED</div>');
+            $.ajax({
+                type: 'PUT',
+                url: '/admin/suspicious_offers/' + offerId + '?category_id=' + categoryId,
+                success: function(data) {
+                    if ( data.status == 'success' ) {
+                        $('#offer-' + offerId).find('.offer-left-side').append('<div style="color:red; font-weight:bold">UPDATED</div>');
 
-                    $.admin.filter.find('input[type="checkbox"]').filter(':checked').prop('checked', false);
+                        $.admin.filter.find('input[type="checkbox"]').filter(':checked').prop('checked', false);
+                    }
                 }
-            }
-        });
+            });
 
-    });
+        });
+    }
 
 });
