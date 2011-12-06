8e83888cdd71dad5dc1d7c85e29aba20f9d5573d
Vlad Khomich <khomich.vlad@gmail.com>
2011-12-06 20:24:48 +0200

    minor fixes

--- app/assets/javascripts/admin/suspicious_offers.js
+++ app/assets/javascripts/admin/suspicious_offers.js
@@ -1,7 +1,7 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
-$('document').ready(function() {
+$('#admin-suspicious_offers-controller').ready(function() {
 
     $.admin = {
         filter: $('#filter')
