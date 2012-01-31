77bacab10cb09fe1a6b4cd5a4259f461c39ab23d
Vlad Khomich <khomich.vlad@gmail.com>
2012-01-31 21:10:23 +0200

    fixes

--- parsers/kupongid.rb
+++ parsers/kupongid.rb
@@ -39,7 +39,7 @@ cities.keys.each do |city|
 
   pages_count.times do
     offset = current_page == 1 ? 0 : current_page * offers_per_page
-    @bot.get cities[city] + '?kuponmap=1&deal_groupID=0&select=select_all&offset=' + offset.to_s
+    @bot.get cities[city] + '?kuponmap=1&deal_groupID=0&select=select_all&offset=' + offset.to_s rescue binding.pry
     current_page += 1
 
     offer_patterns = @bot.page.parser.css('div.coupon')
@@ -51,7 +51,17 @@ cities.keys.each do |city|
       kupongid_provider_id = pattern.css('.source a').first['href'].gsub(/\D/, '').to_i
       url = pattern.css('.description h2 a.local').first['href']
       provided_id = url.gsub(/\D/, '')
-      description = pattern.css('.coupon-popup-tab').first.css('p')
+      raw_description = pattern.css('.coupon-popup-tab').first
+
+      raw_description.css('a').each do |a|
+        if a['href'] =~ /kupongid/
+          a.remove
+        else
+          a['target'] = 'blank'
+          a['rel'] = 'nofollow'
+        end
+      end
+
 
       if existing_offers.include?(provided_id) || saved_offers.include?(provided_id)
         existing_offers.delete( provided_id )
@@ -67,13 +77,24 @@ cities.keys.each do |city|
         next
       end
 
+      provider_url = begin
+                       out_page = @bot.get(pattern.css(".buy").first['href'])
+                       if out_page.uri.to_s =~ /kupongid/
+                         out_page.links.last.href.gsub /\?.*/, ''
+                       else
+                         out_page.uri.to_s.gsub /\?.*/, ''
+                       end
+                     rescue Exception => e
+                       nil
+                     end
+
       offer = {
-        url: pattern.css('.description h2 a.local').first['href'],
+        url: provider_url,
         title: pattern.css('.description h2 a.local').text,
-        provided_id: pattern.css('.description h2 a.local').first['href'].gsub(/\D/, ''),
+        provided_id: url.gsub(/\D/, ''),
         image: open( pattern.css('.image img').first['src'].gsub('_small', '') ),
-        description: description.count == 1 ? description.text : description[1].text,
-        ends_at: pattern.css('.time').text =~ /\dд/ ? pattern.css('.time').text.gsub(/д.*/, '').to_i.days.from_now : Time.now,
+        description: raw_description.css('p').count == 1 ? raw_description.text.gsub(/\n|\t|\r\n/, '') : raw_description.css('p')[1].text.gsub(/\n|\t|\r\n/, ''),
+        ends_at: pattern.css('.time').text =~ /\dд/ ? pattern.css('.time').text.gsub(/д.*/, '').to_i.days.from_now : nil,
         subway: pattern.css('.location noindex b').text,
         address: address.count > 0 ? address[1].gsub(/Захотели купить.*/, '').strip : nil,
         price: pattern.css('.price-and-time strong').text.to_i,
@@ -86,7 +107,7 @@ cities.keys.each do |city|
       }
 
       model = Offer.new( offer )
-      #binding.pry
+      binding.pry
       if model.valid?
         @log.info("Saving offer #{model.provided_id}")
         city_clone = city.clone
