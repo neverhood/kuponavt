cbfc4a1b9a3ff09a39efab65127eee07f45c183b
Vlad Khomich <khomich.vlad@gmail.com>
2012-02-06 13:59:02 +0200

    favourites fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  before_filter :refresh_cookies
+
   helper_method :russian_cities, :ukrainian_cities
 
   def russian_cities
@@ -9,5 +11,17 @@ class ApplicationController < ActionController::Base
 
   def ukrainian_cities
     Country.find_by_name('ukraine').cities
+  end
+
+  private
+
+  def refresh_cookies
+    return unless cookies['favourites']
+
+    favourites = []
+    cookies['favourites'].split(',').each do |favourited|
+      favourites << favourited if Offer.where(id: favourited.gsub(/_.*/, '')).count > 0
+    end
+    cookies['favourites'] = favourites.join(',')
   end
 end
